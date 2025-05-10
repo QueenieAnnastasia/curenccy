c62c8116820ea7f29df8068d9f5cd0bab6266c46
QueenieAnnastasia <94047113+QueenieAnnastasia@users.noreply.github.com>
2025-05-10 23:58:48 +0700

    Bài tập chương trình đổi tiền

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhập số tiền $ cần chuyển đổi giá trị ");
+        double USD = Convert.ToDouble(Console.ReadLine());
+        const int curenccyRate = 25000;
+        double VND = USD*curenccyRate;
+        Console.WriteLine("Giá trị tiền Việt Nam là " + VND + " đồng");
+
+    }
+}
